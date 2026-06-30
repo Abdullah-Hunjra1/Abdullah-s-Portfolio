ba9e579bee102e56a9bdb4a36b2236fa7edad6de
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2026-07-01 02:12:24 +0500

    Dock section completed

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 import Navbar from '#components/Navbar.jsx'
 import Welcome from '#components/Welcome'
+import Dock from '#components/Dock'
 
 export {
     Navbar,
-    Welcome
+    Welcome,
+    Dock
 }
