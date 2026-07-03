b5613f6e377ee085baf570ddf58df9f5778b4fe4
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2026-07-03 23:38:18 +0500

    Text & Image File completed

--- src/windows/index.js
+++ src/windows/index.js
@@ -2,6 +2,8 @@ import Terminal from "#windows/Terminal.jsx";
 import Safari from "#windows/Safari.jsx";
 import Resume from "#windows/Resume.jsx";
 import Finder from "#windows/Finder.jsx";
+import Text from "#windows/Text.jsx";
+import Image from "#windows/Image.jsx";
 
 
 
@@ -9,5 +11,7 @@ export {
     Terminal,
     Safari,
     Resume,
-    Finder
+    Finder,
+    Text,
+    Image
 }
