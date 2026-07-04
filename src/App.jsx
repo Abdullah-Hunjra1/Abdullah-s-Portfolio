04dc933fc8a7a7d26e878c44eeb91aa0bc523cf9
Abdullah-Hunjra1 <abdullahhunjra111@gmail.com>
2026-07-04 16:58:25 +0500

    Home Section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from 'gsap'
 import { Draggable } from 'gsap/Draggable'
 
-import { Dock, Navbar, Welcome } from '#components'
+import { Dock, Home, Navbar, Welcome } from '#components'
 import { Contact, Finder, Image, Resume, Safari, Terminal, Text } from '#windows'
 
 
@@ -13,6 +13,7 @@ const App = () => {
       <Navbar />
       <Welcome />
       <Dock />
+      <Home />
 
       <Terminal />
       <Safari />
